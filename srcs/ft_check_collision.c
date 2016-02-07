484839a961bc2a49b219e450ba83a81b57d41f5d
cledant <cledant@student.42.fr>
2016-02-07 23:03:18 +0100

    erreur de recul quand coller au mur corriger

--- srcs/ft_check_collision.c
+++ srcs/ft_check_collision.c
@@ -6,7 +6,7 @@
 /*   By: cledant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/07 16:26:38 by cledant           #+#    #+#             */
-/*   Updated: 2016/02/07 22:53:20 by cledant          ###   ########.fr       */
+/*   Updated: 2016/02/07 23:02:29 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,7 @@ int		ft_check_collision(t_mlx *e, double angle)
 	value[0] = angle + FOV / 2;
 	value [1] = FOV / 2;
 	if (ft_select_ray(e, value, e->x_player, e->y_player) > 10)
-	{
-		value[0] = angle;
-		value[1] = 0;
-		if (ft_select_ray(e, value, e->x_player, e->y_player) > 10)
-		{
-			value[0] = angle - FOV / 2;
-			value[1] = -FOV / 2;
-			if (ft_select_ray(e, value, e->x_player, e->y_player) > 10)
 				return (1);
-			else
-				return (0);
-		}
-		else
-			return (0);
-	}
 	else
 		return (0);
 }
